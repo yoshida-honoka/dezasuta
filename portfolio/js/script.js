4a2fbd0837fec0d8f1c25283ca4b51521b122c15
yoshida-honoka <148866174+yoshida-honoka@users.noreply.github.com>
2024-02-06 21:50:27 +0900

    portfolio

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -35,4 +35,77 @@ $('.gallery_wrap').slick({
             }
 
         }]
-})
+});
+
+$(function(){
+    setTimeout(function(){
+        // ページが読み込まれて3秒後にここに記述された処理が発動します。
+        $('.loading').fadeOut(1000);
+      }, 3000);
+    });
+
+    const webStorage = function () {
+        if (sessionStorage.getItem('visit')) {
+          $(".loading").css("display", "none");
+        } else {
+          sessionStorage.setItem('visit', 'true'); 
+          setTimeout(function(){
+            // ページが読み込まれて3秒後にここに記述された処理が発動します。
+            $('.loading').fadeOut(1000);
+          }, 3000);
+        };
+      };
+      webStorage();
+
+// $(function () {s
+//     function end_loader() {
+//       $('.loading').fadeOut(800);
+//     }
+//     $(window).on('load', function () {
+//       setTimeout(function () {
+//         end_loader();
+//       }, 3000)
+//     })
+//   });
+
+//テキストのカウントアップの設定
+var bar = new ProgressBar.Line(splash_text, {//id名を指定
+	strokeWidth: 0,//進捗ゲージの太さ
+	duration: 3000,//時間指定(1000＝1秒)
+	trailWidth: 0,//線の太さ
+	text: {//テキストの形状を直接指定	
+		style: {//天地中央に配置
+			position:'absolute',
+			left:'50%',
+			top:'50%',
+			padding:'0',
+			margin:'0',
+			transform:'translate(-50%,-50%)',
+			'font-size':'1.2rem',
+			color:'#333',
+		},
+		autoStyleContainer: false //自動付与のスタイルを切る
+	},
+	step: function(state, bar) {
+		bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+	}
+});
+
+//アニメーションスタート
+bar.animate(1.0, function () {//バーを描画する割合を指定します 1.0 なら100%まで描画します
+	$("#splash").delay(500).fadeOut(800);//アニメーションが終わったら#splashエリアをフェードアウト
+}); 
+
+$(function(){
+    $(window).on('load scroll', function() {
+       var winScroll = $(window).scrollTop();
+       var winHeight = $(window).height();
+       var scrollPos = winScroll + (winHeight * 0.8);
+ 
+       $(".show").each(function() {
+          if($(this).offset().top < scrollPos) {
+             $(this).css({opacity: 1, transform: 'translate(0, 0)'});
+          }
+       });
+    });
+ });
